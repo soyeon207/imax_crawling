52ef6591d25fe49d9252eb7291f4c91979bd6d1a
soyeon207 <psy010207@naver.com>
2020-04-09 03:21:19 +0900

    requests로 페이지 정보 가져오기

---
+++ crawling_first.py
@@ -0,0 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20190602'
+html = requests.get(url)
+print(html.text)
